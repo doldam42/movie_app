7e203b078c230459d66535202a7e85c5244ac020
doldam42 <doldam42@github.com>
2020-12-30 15:00:42 +0000

    movie data fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import react from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends react.Component {
 	state = {
-		isLoading: true
+		isLoading: true,
+		movies: [],
 	};
-	
+	getMovies = async () => {
+		const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+	}
 	componentDidMount() {
-		setTimeout(() => {
-			this.setState({ isLoading: false });
-		}, 6000);
+		this.getMovies();
 	}
 	render() {
 		const { isLoading } = this.state;
